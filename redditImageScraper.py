e2d4b4bf2d0517da91ac5e8a36c09d69a5449bbe
Ahmed-Z <ahmed.veno@gmail.com>
2022-11-06 18:39:46 +0000

    refactor code to flask app

--- redditImageScraper.py
+++ redditImageScraper.py
@@ -4,19 +4,20 @@ import re
 import requests
 import os
 import concurrent.futures
-from dotenv import dotenv_values
+from flask import Flask, render_template, request   
+# from dotenv import dotenv_values
 
 
 class redditImageScraper:
-    def __init__(self, sub):
-        config = dotenv_values(".env")
-        self.sub = sub
+    def __init__(self, data):
+        self.sub = data["subreddit"]
+        self.sort = data["sort"].lower()
         self.extension = ['jpeg', 'jpg', 'png']
-        self.reddit = praw.Reddit(client_id=config["C_ID"],
-                                  client_secret=config["C_SECRET"],
-                                  password=config["PASSWORD"],
-                                  user_agent=config["UA"],
-                                  username=config["USERNAME"])
+        self.reddit = praw.Reddit(client_id=data["client_username"],
+                                  client_secret=data["client_secret"],
+                                  password=data["reddit_password"],
+                                  user_agent = "whatever",
+                                  username=data["reddit_username"])
 
     def writeFile(self, link):
         name = re.search('(?s:.*)\w/(.*)', link)
@@ -26,44 +27,35 @@ class redditImageScraper:
             with open(name, 'wb') as f:
                 f.write(request.content)
 
-    def start(self, sort):
+    def start(self):
         subreddit = self.reddit.subreddit(self.sub)
-        if sort == "hot":
+        if self.sort == "hot":
             submissions = subreddit.hot()
-        if sort == "new":
+        if self.sort == "new":
             submissions = subreddit.new()
-        if sort == "controversial":
+        if self.sort == "controversial":
             submissions = subreddit.controversial()
-        if sort == "rising":
+        if self.sort == "rising":
             submissions = subreddit.rising()
-        if sort == "top":
+        if self.sort == "top":
             submissions = subreddit.top()
-        if sort == "gilded":
+        if self.sort == "gilded":
             submissions = subreddit.gilded()
         imageLinks = []
         print("[+] Fetching image links")
-        try:
-            for submission in submissions:
-                if submission.url[-3:] in self.extension or submission.url[-4:] in self.extension:
-                    imageLinks.append(submission.url)
-        except Exception as e:
-            exit("[!] " + self.sub + " does not exist")
 
-        print("[+] Found {} in {}".format(len(imageLinks), sort))
-        limit = str(input("[+] Download ['all'/limit]: "))
-        while True:
-            try:
-                if limit == "all" or int(limit) >= len(imageLinks):
-                    l = len(imageLinks)
-                    break
-                else:
-                    l = int(limit)
-                    break
-            except:
-                print("[!] Please enter a valid input ('all' to download all images)")
+        for submission in submissions:
+            if submission.url[-3:] in self.extension or submission.url[-4:] in self.extension:
+                imageLinks.append(submission.url)
+
+        return imageLinks
+        print("[+] Found {} in {}".format(len(imageLinks), self.sort))
+        # limit = str(input("[+] Download ['all'/limit]: "))
+        limit = 99
+        
         print(
-            "[+] Downloading {} images from {} using {} sort".format(l, self.sub, sort))
-        imageLinks = imageLinks[:l]
+            "[+] Downloading {} images from {} using {} sort".format(limit, self.sub, self.sort))
+        imageLinks = imageLinks[:limit]
         path = './scraped/' + self.sub
         if not os.path.exists(path):
             os.mkdir(path)
@@ -73,14 +65,34 @@ class redditImageScraper:
         print("[+] Download complete. Check ", path)
 
 
-subreddit = str(input("[+] Select the name of the subreddit to scrape: "))
+# subreddit = str(input("[+] Select the name of the subreddit to scrape: "))
 
-sort = ["controversial", "gilded", "hot", "new", "rising", "top"]
-print("[+] How to sort submission?")
-print("- controversial: 0\n- gilded: 1\n- hot: 2\n- new: 3\n- rising: 4\n- top: 5")
-while True:
-    s = int(input("[+] Please choose a number (0->5): "))
-    if s >= 0 and s <= 5:
-        break
-scraper = redditImageScraper(subreddit)
-scraper.start(sort[s])
+# sort = ["controversial", "gilded", "hot", "new", "rising", "top"]
+# print("[+] How to sort submission?")
+# print("- controversial: 0\n- gilded: 1\n- hot: 2\n- new: 3\n- rising: 4\n- top: 5")
+# while True:
+#     s = int(input("[+] Please choose a number (0->5): "))
+#     if s >= 0 and s <= 5:
+#         break
+
+app = Flask(__name__,template_folder='./templates')
+pics = []
+@app.route('/')
+def index():
+    return render_template('./index.html',pics=pics)
+
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    global pics
+    print(request.form.to_dict())
+    data = request.form.to_dict()
+    scraper = redditImageScraper(data)
+    imagelinks = scraper.start()
+    pics = imagelinks    
+    return render_template('./index.html',pics=pics)
+
+app.run()
+
+
+#scraper.start(sort[s])
