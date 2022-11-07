12d28719713084858e2b40690ac72ec9ebb7c8e9
Ahmed-Z <ahmed.veno@gmail.com>
2022-11-07 15:10:53 +0100

    UI update & API update

--- redditImageScraper.py
+++ redditImageScraper.py
@@ -5,7 +5,6 @@ import requests
 import os
 import concurrent.futures
 from flask import Flask, render_template, request   
-# from dotenv import dotenv_values
 
 
 class redditImageScraper:
@@ -16,17 +15,8 @@ class redditImageScraper:
         self.reddit = praw.Reddit(client_id=data["client_username"],
                                   client_secret=data["client_secret"],
                                   password=data["reddit_password"],
-                                  user_agent = "whatever",
+                                  user_agent = "Mozilla/5.0 (Linux; ; )AppleWebKit/ (KHTML, like Gecko) Chrome/Mobile Safari/",
                                   username=data["reddit_username"])
-
-    def writeFile(self, link):
-        name = re.search('(?s:.*)\w/(.*)', link)
-        name = name.group(1)
-        request = requests.get(link)
-        if not os.path.isfile(name):
-            with open(name, 'wb') as f:
-                f.write(request.content)
-
     def start(self):
         subreddit = self.reddit.subreddit(self.sub)
         if self.sort == "hot":
@@ -42,57 +32,64 @@ class redditImageScraper:
         if self.sort == "gilded":
             submissions = subreddit.gilded()
         imageLinks = []
-        print("[+] Fetching image links")
 
         for submission in submissions:
             if submission.url[-3:] in self.extension or submission.url[-4:] in self.extension:
                 imageLinks.append(submission.url)
-
         return imageLinks
-        print("[+] Found {} in {}".format(len(imageLinks), self.sort))
-        # limit = str(input("[+] Download ['all'/limit]: "))
-        limit = 99
-        
-        print(
-            "[+] Downloading {} images from {} using {} sort".format(limit, self.sub, self.sort))
-        imageLinks = imageLinks[:limit]
-        path = './scraped/' + self.sub
-        if not os.path.exists(path):
-            os.mkdir(path)
-        os.chdir(path)
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-            executor.map(self.writeFile, imageLinks)
-        print("[+] Download complete. Check ", path)
 
-
-# subreddit = str(input("[+] Select the name of the subreddit to scrape: "))
-
-# sort = ["controversial", "gilded", "hot", "new", "rising", "top"]
-# print("[+] How to sort submission?")
-# print("- controversial: 0\n- gilded: 1\n- hot: 2\n- new: 3\n- rising: 4\n- top: 5")
-# while True:
-#     s = int(input("[+] Please choose a number (0->5): "))
-#     if s >= 0 and s <= 5:
-#         break
 
 app = Flask(__name__,template_folder='./templates')
+
+# GLOBALS
 pics = []
+subreddit = ''
+
 @app.route('/')
 def index():
-    return render_template('./index.html',pics=pics)
+    download_button = "hidden"
+    return render_template('./index.html',pics=pics,download_button=download_button)
 
 
 @app.route('/submit', methods=['POST'])
 def submit():
-    global pics
-    print(request.form.to_dict())
+    global pics, subreddit
     data = request.form.to_dict()
+    subreddit = data["subreddit"]
     scraper = redditImageScraper(data)
-    imagelinks = scraper.start()
-    pics = imagelinks    
-    return render_template('./index.html',pics=pics)
+    try:
+        imagelinks = scraper.start()
+    except Exception as e:
+        error="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative ml-8 mr-8"
+        return render_template('./index.html',pics=pics,error=error,exception=e,download_button="hidden")
 
+    pics = imagelinks
+    info = "flex items-center bg-blue-500 text-white text-sm font-bold px-4 py-3 ml-8 mr-8"
+    message = "Found " + str(len(imagelinks)) + " images."
+    download_button = "flex-shrink-0 bottom-0 right-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 mt-8 text-sm border-4 text-white py-1 px-2 rounded"    
+    return render_template('./index.html',pics=pics,download_button=download_button,info=info,message=message)
+
+@app.route('/download', methods=['POST'])
+def download():
+    global pics
+    def writeFile(link):
+        name = re.search('(?s:.*)\w/(.*)', link)
+        name = name.group(1)
+        request = requests.get(link)
+        if not os.path.isfile(name):
+            with open(name, 'wb') as f:
+                f.write(request.content)
+
+    path = os.path.join("scraped",subreddit)
+    if not os.path.exists(path):
+        os.mkdir(path)
+    os.chdir(path)
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        executor.map(writeFile, pics)
+    download_button = "hidden"
+    info = "flex items-center bg-blue-500 text-white text-sm font-bold px-4 py-3 ml-8 mr-8"
+    message = str(len(pics)) + " images downloaded successfully in ." + path
+    return render_template('./index.html',pics=pics,download_button=download_button,d_info=info,d_message=message)
 app.run()
 
 
-#scraper.start(sort[s])
